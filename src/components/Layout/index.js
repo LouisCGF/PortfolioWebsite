14e55bca00c1d6cab3d4ac608d3091cff4260203
Louis Ware <louisware@Soliriun197sMBP.localdomain>
2024-01-04 15:17:58 +0000

    Built site layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,27 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () => {
     return (
-        <>
-            <Sidebar/>
-        </>
+        <div className="App">
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>if page == "Home" &#123;</span> { /* &lt;body&gt;  <-- this is to display <body>. Replace 'body' with 'html' for html */ }
+
+                <span className='tags under-top'>displayPage();</span>
+
+                <br/>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>
+                &#125;
+                <br/>
+                </span>
+
+            </div>
+        </div>
     )
 }
 
